d7004329b282eee942ee9bb981a613331cdb9f44
KrunitMistry <krunitmistry2003@gmail.com>
2025-03-17 12:33:50 +0530

    Update Model.py

--- Model.py
+++ Model.py
@@ -21,7 +21,7 @@ st.caption(
 )
 
 # Define the model file path
-modelfile = "D:\Internship_Course\Week 06/voting_model.pkl"
+modelfile = "/voting_model.pkl"
 
 # Load the trained model with caching for faster performance
 @st.cache_resource
